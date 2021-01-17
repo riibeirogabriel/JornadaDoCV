ac1d6b830daa88bd73290ff023ad1416ebaf519e
Gustavo Faria <gustavo@lonewolfcorp.com>
2021-01-17 14:24:04 -0300

    Private router

--- src/layout/Private/index.js
+++ src/layout/Private/index.js
@@ -1,7 +1,22 @@
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+
 import * as Styles from './styles';
 
-const Private = ({ children }) => (
-  <Styles.Private>{children}</Styles.Private>
-);
+const Private = ({ children }) => {
+  // const router = useRouter();
+
+  // const isUserLogged = getIdToken();
+
+  // useEffect(() => {
+  //   if (!isUserLogged) router.push('/admin/login');
+  // }, [isUserLogged]);
+
+  // if (!isUserLogged) return null;
+  console.log('');
+  return (
+    <Styles.Private>{children}</Styles.Private>
+  );
+};
 
 export default Private;
